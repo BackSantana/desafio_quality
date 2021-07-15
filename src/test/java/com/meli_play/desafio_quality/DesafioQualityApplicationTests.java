94825f55a24561c13f15adf98d33a104ddb7fc2c
BackSantana <carlos.santana@mercadolivre.com>
2021-07-15 09:36:42 -0300

    First commit - escopo inicial

---
+++ src/test/java/com/meli_play/desafio_quality/DesafioQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli_play.desafio_quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
